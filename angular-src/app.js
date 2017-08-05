7fd953245fb665ff29081b61a18095dfbef8f684
sadeajayi <folasadeajayi@hotmail.com>
2017-08-05 03:32:14 -0400

    app

--- angular-src/app.js
+++ angular-src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/src'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -30,5 +30,5 @@ app.use(forceSSL());
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
